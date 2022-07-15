9b378840521d8207f71139e5eca57e041a4d65f6
Douglas Fernandes <douglasf.filho@gmail.com>
2022-07-15 18:59:44 -0300

    first project configuration

---
+++ src/main/java/br/com/ariany/enhancedstarwarsapi/EnhancedStarWarsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ariany.enhancedstarwarsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhancedStarWarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhancedStarWarsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ariany/enhancedstarwarsapi/EnhancedStarWarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ariany.enhancedstarwarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancedStarWarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
